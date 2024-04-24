4ea74b3112152a9fa6b3789a6f2665f243072964
Eric Peltzman <epeltz@optonline.net>
2024-04-24 10:40:15 -0400

    5.1 updated hand descr

--- src/main/java/gameoutput/GameFile.java
+++ src/main/java/gameoutput/GameFile.java
@@ -9,7 +9,8 @@ public class GameFile {
     public static void writeCSVData(String filename, Player player, int winAmount) {
         try (PrintWriter output = new PrintWriter(new FileWriter("files/" + filename))) {
             output.println("Player ID, Player Name, Hand, Hand Descr, Win Amount, Bank");
-            output.println(player.getId() + "," + player.getName() + "," + player.getHand() + "," + player.getHand().getHandString() + "," + winAmount + "," + player.getBank());
+            output.println(player.getId() + "," + player.getName() + "," + player.getHand() + "," +
+                    player.getHand().getHandDescr() + "," + winAmount + "," + player.getBank());
         } catch (IOException e) {
             System.out.println("An error occurred while writing to the file: " + e.getMessage());
         }
